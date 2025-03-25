c2725033dddeafb51eda6b26fc0f6df483519cde
abdul rasheed <abdul.rasheed@simpleenergy.in>
2025-03-25 13:14:54 +0530

    arrays and non-nullability

--- resolvers.js
+++ resolvers.js
@@ -1,11 +1,19 @@
 export const resolvers = {
     Query: {
-        job: () => {
-            return {
-                id: 'test-id',
-                title: 'the title',
-                description: 'the description'
-            }
+        jobs: () => {
+            return [
+                {
+                    id: 'test-id 1',
+                    title: 'the title 1',
+                    description: 'the description'
+                },
+                {
+                    id: 'test-id 2',
+                    title: 'the title 2',
+                    description: 'the description'
+                },
+                // null
+            ]
         }
     }
 }
